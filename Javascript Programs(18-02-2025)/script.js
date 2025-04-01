c8bce82352b4eebddcad32cb4df001d7c9dca875
Pawan-8766 <gandaspawan1@gmail.com>
2025-04-01 23:30:05 +0530

    Delete Javascript Programs(18-02-2025) directory

--- Javascript Programs(18-02-2025)/script.js
+++
@@ -1,9 +0,0 @@
-function findLargest(num1, num2) {
-    if (num1 > num2) {
-        return num1;
-    } else {
-        return num2;
-    }
-}
-
-console.log(findLargest(10, 20));
--- Javascript Programs(18-02-2025)/script4.js
+++
@@ -1,7 +0,0 @@
-function multiplicationTable(number) {
-    for (let i = 1; i <= 10; i++) {
-        console.log(`${number} * ${i} = ${number * i}`);
-    }
-}
-
-multiplicationTable(5);
